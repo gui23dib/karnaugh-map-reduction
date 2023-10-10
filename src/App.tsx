026e41e959b08ee11d4b1b15c797282c8df34ddb
Thiago Prado <103077237+ThiagoAPrado@users.noreply.github.com>
2023-10-09 22:27:22 -0300

    start mapa de karnaugh

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Title from './assets/components/Title'
+
+function App() {
+
+  return (
+    <>
+      <Title/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Title.tsx
@@ -0,0 +1,7 @@
+const Title = () => {
+  return (
+    <h1>Mapa de karnaugh</h1>
+  )
+}
+
+export default Title
