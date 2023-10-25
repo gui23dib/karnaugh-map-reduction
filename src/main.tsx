887126ec2e7cb0669159a76614c0bc787bcb8502
gui23dib <gui23dib@gmail.com>
2023-10-24 23:39:40 -0300

    config deploy w/ gatsby

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<div id='root'><App /></div>);
